dff0fc3999794b8ed46b6f78977ad9ace6d978c5
Sahil-Bista <160755798+Sahil-Bista@users.noreply.github.com>
2024-08-19 11:07:35 +0545

    first commit

---
+++ models/user.js
@@ -0,0 +1,11 @@
+// module.exports = (sequelize, DataTypes) => {
+//     const User = sequelize.define("user",{
+//         id:{
+//             type : DataTypes.INTEGER,
+//             primaryKey : true,
+//             autoIncrement : true,
+//             allowNull : false
+//         },
+//         username:{}
+//     })
+// }
